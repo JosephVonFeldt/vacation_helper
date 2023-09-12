47b401e214f71e940976f66451012ad3cb66ba72
Joey <joey.vonfeldt@gmail.com>
2023-09-12 08:53:18 -0700

    Small page update

--- src/app.py
+++ src/app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def main():
     return '''
      <form action="/echo_user_input" method="POST">
+         <select name="city" id="city-select">
+          <option value="">--Please choose an option--</option>
+          <option value="Boulder">Boulder</option>
+          <option value="Denver">Denver</option>
+          <option value="New York">New York</option>
+         </select>
          <input name="user_input">
          <input type="submit" value="Submit!">
      </form>
@@ -13,5 +19,6 @@ def main():
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
+    city = request.form.get("city", "")
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    return "You entered: " + input_text + " "+ city
