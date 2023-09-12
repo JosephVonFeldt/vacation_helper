1fbb88bf8e7301134a7169fb625d7386c3d6edce
Joey <joey.vonfeldt@gmail.com>
2023-09-12 09:25:56 -0700

    Updating page

--- src/app.py
+++ src/app.py
@@ -7,18 +7,40 @@ def main():
     return '''
      <form action="/echo_user_input" method="POST">
          <select name="city" id="city-select">
-          <option value="">--Please choose an option--</option>
-          <option value="Boulder">Boulder</option>
-          <option value="Denver">Denver</option>
+          <option value="">--Please choose a Departure City--</option>
+          <option value="Los Angeles">Los Angeles</option>
+          <option value="Madison">Madison</option>
           <option value="New York">New York</option>
          </select>
-         <input name="user_input">
-         <input type="submit" value="Submit!">
+         <br>
+         Vacation Type: <br>
+         <input type="radio" id="Snow" name="vacation" value="Snow" checked >
+            <label for="Snow">Snow</label><br>
+            <input type="radio" id="Beach" name="vacation" value="Beach">
+            <label for="Beach">Beach</label><br>
+            <input type="radio" id="Hiking" name="vacation" value="Hiking">
+            <label for="Hiking">Hiking</label>
+         <br>
+         <input name="user_input" placeholder="Text to Echo">
+         <input type="submit" value="Submit">
      </form>
      '''
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     city = request.form.get("city", "")
+    vacation_type = request.form.get("vacation", "")
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text + " "+ city
+    return f"User Input Echo: {input_text} <br> " \
+           f"Start City: {city} <br> " \
+           f"Suggested Destination City: {calcCity(vacation_type)} "
+
+def calcCity(vacation_type):
+    if vacation_type == "Snow":
+        return "Denver"
+    elif vacation_type == "Beach":
+        return "San Diego"
+    elif vacation_type == "Hiking":
+        return "Salk Lake City"
+    else:
+        return "New York"
